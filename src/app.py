503bf703d71c82f26dea7c11b73b87b4d7c4e1e4
SneakyStarling <nope@example.com>
2025-04-20 22:15:33 +0200

    missing resource bugfix

--- src/app.py
+++ src/app.py
@@ -130,14 +130,13 @@ class App:
         logger.setup_logger(log_level)
 
     def start(self, config_file: str) -> None:
-        self.setup_logging()
-        logger.log_debug(f"Artie Scraper v{VERSION}")
         self.gui.draw_start()
         self.gui.screen_reset()
-        input.open_input_device()
+        self.setup_logging()
+        logger.log_debug(f"Artie Scraper v{VERSION}")
+        self.load_config(config_file)
         main_gui = self.gui.create_image()
         self.gui.draw_active(main_gui)
-        self.load_config(config_file)
         self.load_emulators()
 
     def update(self) -> None:
