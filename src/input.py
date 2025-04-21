52b3309673415f3f04d56b7283de524eed88dc55
SneakyStarling <nope@example.com>
2025-04-21 10:49:58 +0200

    bugfix

--- src/input.py
+++ src/input.py
@@ -84,7 +84,7 @@ def key_pressed(code, key_value=1):
     with input_lock:
         if (code, key_value) in active_buttons:
             if key_value == 0:
-                del active_buttons[key_value]
+                del active_buttons[(code, key_value)]
             return True
         return False
 
